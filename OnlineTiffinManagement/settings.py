968b6570d466a0f85e1c0eb258dc4cf33fee0e84
Mohit kumar <125747926+Mohitk001@users.noreply.github.com>
2025-04-06 23:10:36 +0530

    Update settings.py

--- OnlineTiffinManagement/settings.py
+++ OnlineTiffinManagement/settings.py
@@ -51,7 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'OnlineTiffinManagement.urls'
 
@@ -121,17 +123,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 # For static files like CSS, JS
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # if you have a static folder in root
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # used by collectstatic in production
+
+  # used by collectstatic in production
 
 # For media files like uploaded images
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static")
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # for dev
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
